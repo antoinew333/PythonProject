a315d5a78bfacf85618ce15f51c0429aa73e4ec0
antoinew333 <wieczorekantek@outlook.com>
2025-10-30 21:43:43 +0100

    initial commit

--- biblioteka/pages/dodaj.py
+++ biblioteka/pages/dodaj.py
@@ -6,6 +6,18 @@ def zapisz_ksiazke(ksiazki):
     json.dump(ksiazki, plik, indent = 2)
     plik.close()
 
+def wczytaj_ksiazki():
+    try:
+        with open("ksiazki.json", "r") as plik:
+            ksiazki = json.load(plik)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("FileNotFoundError")
+        ksiazki = []
+    return ksiazki
+
+if "ksiazki" not in st.session_state:
+    st.session_state.ksiazki = wczytaj_ksiazki()
+
 st.set_page_config(initial_sidebar_state="collapsed")
 
 st.session_state["dodawanie"] = True
