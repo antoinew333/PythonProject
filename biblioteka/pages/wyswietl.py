b9602b612482952b073c3e20477897ce8b9556af
antoinew333 <wieczorekantek@outlook.com>
2025-10-31 23:41:32 +0100

    initial commit

--- biblioteka/pages/wyswietl.py
+++ biblioteka/pages/wyswietl.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import json
-
+import pandas as pd
 def wczytaj_ksiazki():
     try:
         with open("ksiazki.json", "r") as plik:
@@ -16,7 +16,13 @@ if "ksiazki" not in st.session_state:
     st.session_state.ksiazki = wczytaj_ksiazki()
 
 st.markdown("Lista książek:")
-st.session_state.ksiazki.index(range(1, len(st.session_state.ksiazki) + 1))
-st.dataframe(st.session_state.ksiazki)
+
+if len(st.session_state.ksiazki) > 0:
+    x = pd.DataFrame(st.session_state.ksiazki)
+    x.index = range(1, len(x) + 1)
+    x.index.name = "Lp."
+    st.dataframe(x)
+else:
+    st.info("Brak książek w bibliotece.")
 
 st.page_link("main.py", label="Powrót do MENU")
