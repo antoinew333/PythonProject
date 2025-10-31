d4ea687e964dd2dc6e9df4d6227a3fb133336f4e
antoinew333 <wieczorekantek@outlook.com>
2025-10-31 23:24:50 +0100

    initial commit

--- biblioteka/pages/wyswietl.py
+++ biblioteka/pages/wyswietl.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import json
+from numpy.random import default_rng as rng
 
 def wczytaj_ksiazki():
     try:
@@ -16,7 +17,6 @@ if "ksiazki" not in st.session_state:
     st.session_state.ksiazki = wczytaj_ksiazki()
 
 st.markdown("Lista książek:")
-for x in range(len(st.dataframe(st.session_state.ksiazki))):
-    print(x + 1)
+st.dataframe(st.session_state.ksiazki, rng(0).standard_normal((0,0)))
 
 st.page_link("main.py", label="Powrót do MENU")
