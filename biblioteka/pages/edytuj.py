5b1e429ed2287fc4cd44522130aa0aa599b92520
antoinew333 <wieczorekantek@outlook.com>
2025-11-01 18:58:08 +0100

    initial commit

--- biblioteka/pages/edytuj.py
+++ biblioteka/pages/edytuj.py
@@ -28,23 +28,25 @@ st.session_state["edytowanie"] = True
 pozycje = [x["Tytuł"] for x in st.session_state.ksiazki]
 edycja = st.selectbox("Wybierz książkę do edytowania: ", options=pozycje)
 
+ksiazka = next((x for x in st.session_state.ksiazki if x["Tytuł"] == edycja), None)
+
 if st.session_state["edytowanie"]:
-    tytul = st.text_input("Tytuł: ", pozycje["Tytuł"])
-    autor = st.text_input("Autor: ", pozycje["Autor"])
-    wydawnictwo = st.text_input("Wydawnictwo: ", pozycje["Wydawnictwo"])
-    rok = st.text_input("Rok: ", pozycje.get("Rok", ""))
-    status = st.checkbox("Przeczytana", value=pozycje.get("Przeczytana", False))
+    tytul = st.text_input("Tytuł: ", ksiazka["Tytuł"])
+    autor = st.text_input("Autor: ", ksiazka["Autor"])
+    wydawnictwo = st.text_input("Wydawnictwo: ", ksiazka["Wydawnictwo"])
+    rok = st.text_input("Rok: ", ksiazka.get("Rok", ""))
+    status = st.checkbox("Przeczytana", value=ksiazka.get("Przeczytana", False))
     if status == "przeczytana":
         status = True
     elif status == "nieprzeczytana":
         status = False
 
     if st.button("Zapisz"):
-        pozycje["Tytuł"] = tytul
-        pozycje["Autor"] = autor
-        pozycje["Wydawnictwo"] = wydawnictwo
-        pozycje["Rok"] = rok
-        pozycje["Przeczytana"] = status
+        ksiazka["Tytuł"] = tytul
+        ksiazka["Autor"] = autor
+        ksiazka["Wydawnictwo"] = wydawnictwo
+        ksiazka["Rok"] = rok
+        ksiazka["Przeczytana"] = status
         zapisz_ksiazke(st.session_state.ksiazki)
         st.session_state.edytowanie = False
         st.success("Książka została zedytowana")
