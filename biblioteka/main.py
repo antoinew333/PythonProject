267e103a1e0b9a0a7d38e063c0a20c85592af467
antoinew333 <wieczorekantek@outlook.com>
2025-11-01 21:35:42 +0100

    initial commit

--- biblioteka/main.py
+++ biblioteka/main.py
@@ -10,11 +10,45 @@ def wczytaj_ksiazki():
         ksiazki = []
     return ksiazki
 
+def zapisz_ksiazke(ksiazki):
+    plik = open("ksiazki.json", "w")
+    json.dump(ksiazki, plik, indent = 2)
+    plik.close()
+
+if "ksiazki" not in st.session_state:
+    st.session_state.ksiazki = wczytaj_ksiazki()
+
 st.set_page_config(initial_sidebar_state="collapsed")
 
 st.title(":blue[BIBLIOTEKA]", width="content")
 
-st.page_link("pages/dodaj.py", label="Dodaj książkę", width="content")
+@st.dialog("Dodaj książkę")
+def ksiazka():
+    if st.session_state["dodawanie"]:
+        tytul = st.text_input("Tytuł: ")
+        autor = st.text_input("Autor: ")
+        wydawnictwo = st.text_input("Wydawnictwo: ")
+        rok = st.text_input("Rok: ")
+        status = st.checkbox("Przeczytana", width="stretch")
+        if status == "przeczytana":
+            status = True
+        elif status == "nieprzeczytana":
+            status = False
+        if st.button("Zapisz książkę"):
+            if len(tytul) != 0:
+                st.session_state.ksiazki.append({"Tytuł": tytul,
+                                                "Autor": autor,
+                                                "Wydawnictwo": wydawnictwo,
+                                                "Rok": rok,
+                                                "Przeczytana": status})
+                zapisz_ksiazke(st.session_state.ksiazki)
+                st.session_state["dodawanie"] = False
+                st.success("Książka została dodana do biblioteki")
+            else:
+                st.warning("Uzupełnij puste pola")
+
+if "ksiazka" not in st.session_state:
+    st.button("Dodaj książkę")
 
 st.page_link("pages/wyswietl.py", label="Wyświetl ksiązkę", width="content")
 
