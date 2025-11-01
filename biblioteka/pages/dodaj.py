10ffd500db8d8dc1b20fb020f43286a1eaa8b337
antoinew333 <wieczorekantek@outlook.com>
2025-11-01 19:20:09 +0100

    initial commit

--- biblioteka/pages/dodaj.py
+++ biblioteka/pages/dodaj.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import json
-import time
 
 def zapisz_ksiazke(ksiazki):
     plik = open("ksiazki.json", "w")
@@ -24,28 +23,28 @@ st.set_page_config(initial_sidebar_state="collapsed")
 st.session_state["dodawanie"] = True
 
 if st.session_state["dodawanie"]:
-    tytul = st.text_input("Tytuł: ")
-    autor = st.text_input("Autor: ")
-    wydawnictwo = st.text_input("Wydawnictwo: ")
-    rok = st.text_input("Rok: ")
-    status = st.checkbox("Przeczytana", width="stretch")
-    if status == "przeczytana":
-        status = True
-    elif status == "nieprzeczytana":
-        status = False
-    if st.button("Zapisz książkę"):
-        if len(tytul) != 0:
-            st.session_state.ksiazki.append({"Tytuł": tytul,
-                                             "Autor":autor,
-                                             "Wydawnictwo": wydawnictwo,
-                                             "Rok": rok,
-                                             "Przeczytana": status})
-            zapisz_ksiazke(st.session_state.ksiazki)
-            st.session_state["dodawanie"] = False
-            st.success("Książka została dodana do biblioteki")
-        else:
-            st.warning("Uzupełnij puste pola")
-        time.sleep(2)
-        st.rerun("dodaj.py")
+    @st.dialog("Dane książki")
+    def ksiazka():
+        tytul = st.text_input("Tytuł: ")
+        autor = st.text_input("Autor: ")
+        wydawnictwo = st.text_input("Wydawnictwo: ")
+        rok = st.text_input("Rok: ")
+        status = st.checkbox("Przeczytana", width="stretch")
+        if status == "przeczytana":
+            status = True
+        elif status == "nieprzeczytana":
+            status = False
+        if st.button("Zapisz książkę"):
+            if len(tytul) != 0:
+                st.session_state.ksiazki.append({"Tytuł": tytul,
+                                                "Autor":autor,
+                                                "Wydawnictwo": wydawnictwo,
+                                                "Rok": rok,
+                                                "Przeczytana": status})
+                zapisz_ksiazke(st.session_state.ksiazki)
+                st.session_state["dodawanie"] = False
+                st.success("Książka została dodana do biblioteki")
+            else:
+                st.warning("Uzupełnij puste pola")
 
 st.page_link("main.py", label="Powrót do MENU")
