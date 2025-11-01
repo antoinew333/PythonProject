adec12fdcd956f76d016b5f6c983aa6ee781213f
antoinew333 <wieczorekantek@outlook.com>
2025-11-01 18:55:10 +0100

    initial commit

--- biblioteka/pages/edytuj.py
+++ biblioteka/pages/edytuj.py
@@ -29,21 +29,22 @@ pozycje = [x["Tytuł"] for x in st.session_state.ksiazki]
 edycja = st.selectbox("Wybierz książkę do edytowania: ", options=pozycje)
 
 if st.session_state["edytowanie"]:
-    tytul = st.text_input("Tytuł: ")
-    autor = st.text_input("Autor: ")
-    wydawnictwo = st.text_input("Wydawnictwo: ")
-    rok = st.text_input("Rok: ")
-    status = st.checkbox("Przeczytana", width="stretch")
+    tytul = st.text_input("Tytuł: ", pozycje["Tytuł"])
+    autor = st.text_input("Autor: ", pozycje["Autor"])
+    wydawnictwo = st.text_input("Wydawnictwo: ", pozycje["Wydawnictwo"])
+    rok = st.text_input("Rok: ", pozycje.get("Rok", ""))
+    status = st.checkbox("Przeczytana", value=pozycje.get("Przeczytana", False))
     if status == "przeczytana":
         status = True
     elif status == "nieprzeczytana":
         status = False
+
     if st.button("Zapisz"):
-        st.session_state.ksiazki.insert({"Tytuł": tytul,
-                                         "Autor": autor,
-                                         "Wydawnictwo": wydawnictwo,
-                                         "Rok": rok,
-                                         "Przeczytana": status})
+        pozycje["Tytuł"] = tytul
+        pozycje["Autor"] = autor
+        pozycje["Wydawnictwo"] = wydawnictwo
+        pozycje["Rok"] = rok
+        pozycje["Przeczytana"] = status
         zapisz_ksiazke(st.session_state.ksiazki)
         st.session_state.edytowanie = False
         st.success("Książka została zedytowana")
