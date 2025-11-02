c729b03c89ab8d1ed601d4a5a3af81110281f1ec
antoinew333 <wieczorekantek@outlook.com>
2025-11-02 15:18:35 +0100

    initial commit

--- biblioteka/main.py
+++ biblioteka/main.py
@@ -4,8 +4,6 @@ st.set_page_config(initial_sidebar_state="collapsed")
 
 st.title(":blue[BIBLIOTEKA]", width="content")
 
-st.image("https://star-wars.pl/grafika/mar2002/jedilib.jpg", width="content")
-
 st.page_link("pages/dodaj.py", label="Dodaj ksiązkę", width="content")
 
 st.page_link("pages/wyswietl.py", label="Wyświetl ksiązkę", width="content")
